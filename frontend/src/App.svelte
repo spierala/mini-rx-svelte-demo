979e0213e94f590a83743bff588a7f3ca32bb223
Florian Spier <mail@florian-spier.de>
2021-08-12 22:25:00 +0200

    Routing

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
+	import { Router, Link, Route } from "svelte-routing";
 	import TodoList from './modules/todo/components/todo-list.svelte';
+	import ProductShell from './modules/product/components/product-shell.svelte';
 </script>
 
-<main>
-	<TodoList/>
-</main>
+<Router url="todos">
+	<nav>
+		<Link to="todos">Todos</Link>
+		<Link to="products">Products</Link>
+	</nav>
+	<div>
+		<Route path="/todos" component="{TodoList}" />
+		<Route path="/products"><ProductShell /></Route>
+	</div>
+</Router>
