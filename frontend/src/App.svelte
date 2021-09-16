a060cae14c12b27672fbaa1dce102247e89372e3
Florian Spier <mail@florian-spier.de>
2021-09-16 20:59:21 +0200

    svelte-spa-router

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,24 +1,21 @@
 <script lang="ts">
-	import { Router, Link, Route } from "svelte-routing";
+	import Router, { link } from 'svelte-spa-router';
+	import active from 'svelte-spa-router/active'
 	import TodoList from './modules/todo/components/todo-list.svelte';
-	import CartList from './modules/cart/components/cart-list.svelte';
 	import ProductShell from './modules/product/components/product-list.svelte';
 	import facade from "./modules/product/state/product-facade.service";
 	import { Observable } from 'rxjs';
 
 	const cartItemsAmount$: Observable<number> = facade.cartItemsAmount$;
 
-	function getProps({ location, href, isPartiallyCurrent, isCurrent }) {
-		const isActive = href === "/" ? isCurrent : isPartiallyCurrent || isCurrent;
-
-		// The object returned here is spread on the anchor element's attributes
-		if (isActive) {
-			return { class: "nav-link active" };
-		}
-		return { class: "nav-link" };
+	const routes = {
+		'/': TodoList,
+		'/products': ProductShell,
+		'*': TodoList,
 	}
 </script>
-<Router url="/">
+
+<body>
 	<div class="w-100 h-100 d-flex flex-column">
 		<nav class="navbar navbar-expand navbar-dark bg-dark">
 			<a class="navbar-brand">
@@ -27,18 +24,15 @@
 			</a>
 			<ul class="d-flex flex-grow-1 navbar-nav mr-auto mt-2 mt-lg-0">
 				<li class="nav-item">
-					<Link to="/" getProps="{getProps}">Todos</Link>
+					<a class="nav-link" href="/" use:link use:active={{className: 'active'}}>Todos</a>
 				</li>
 				<li class="nav-item">
-					<Link to="products" getProps="{getProps}">Products</Link>
+					<a class="nav-link" href="/products" use:link use:link use:active={{className: 'active'}}>Products</a>
 				</li>
 			</ul>
 		</nav>
 		<div class="flex-grow-1">
-			<Route path="/" component="{TodoList}" />
-			<Route path="/products"><ProductShell /></Route>
-			<Route path="/cart"><CartList /></Route>
+			<Router {routes}/>
 		</div>
 	</div>
-</Router>
-
+</body>
