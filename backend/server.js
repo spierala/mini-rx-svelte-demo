746595ce2935e57073ec90b8e8d29ce9ec15a6ca
Florian Spier <mail@florian-spier.de>
2021-09-28 21:10:17 +0200

    Prepare strapi hosting

---
+++ backend/server.js
@@ -0,0 +1,5 @@
+// server.js
+// @see https://strapi.io/documentation/v3.x/getting-started/deployment.html#application-configuration
+const strapi = require('strapi');
+
+strapi(/* {...} */).start();
